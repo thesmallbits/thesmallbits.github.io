d9ab016983f750f56a6473c9b2cb9029bf813ddb
d1vij <verma.divij@gmail.com>
2026-02-24 22:22:36 +0530

    fix2

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 import Menu from "./HeaderMenu";
 
-function HeaderInteractable(props: PropsWithChildren) {
+function HeaderInteractable(props: PropsWithChildren<any>) {
     return (
         <span
             className={cn(
